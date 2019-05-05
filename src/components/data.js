0caf3bd9c1d70a32842f867a4ff3e60ceb0e6483
MarlenaFlueh <m.fluehh@gmail.com>
2019-05-05 22:34:23 +0200

    fix bug

--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const dbApi = "http://192.168.2.115:8086/query?db=noise&q=SELECT*FROM+%22noise_data%22+WHERE room='300' ORDER BY time desc limit 10";
-const raspbiApi = "http://192.168.2.114:5000";
+const raspbiApi = "http://192.168.2.109:5000/";
 
 export const getChartData = async () => {
     const res = await fetch(dbApi);
@@ -9,8 +9,6 @@ export const getChartData = async () => {
 }
 
 export const sendGrenzwert = async (value) => {
-    const res = await axios.post(raspbiApi, {
-        "0": value
-    });
+    const res = await axios.post(raspbiApi + value, {});
     return res.data;
 };
